4ce72adada627bcaedf795fe7a94937e132be354
Anatoil-mu <anatolij180285@gmail.com>
2025-06-16 14:40:07 +0300

    02_lesson

---
+++ 02_lesson/lesson_2_task_4.py
@@ -0,0 +1,18 @@
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+n = int(input("Введите число n: "))
+
+result = fizz_buzz(n)
+
+
+print(f"fizz_buzz: {result}")
